50aa462dfe9b7afa8987adb463b6f3f508d2cb90
chenguihong <chenguihong@haixue.com>
2020-03-27 18:21:58 +0800

    Initial commit

---
+++ src/main/java/com/chengh/hy/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.chengh.hy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @program: hy
+ * @description:
+ * @author: chengh
+ * @create: 2020-03-27 18:17
+ */
+@RestController
+@RequestMapping("/ping")
+public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+}
